ff2eaf123e9780009723f989e579c6bfce1fb42b
HazWahb8080 <hazem.wahb100@gmail.com>
2022-04-13 07:09:13 +0200

    other pages ready

--- pages/about.js
+++ pages/about.js
@@ -2,20 +2,30 @@ import React from 'react'
 import { useRouter } from 'next/router';
 import  dynamic  from 'next/dynamic';
 import { motion } from 'framer-motion';
+import Header from '../components/Header/Header';
+import { useEffect } from 'react';
+import { useState } from 'react';
 const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
   ssr: false
-
 });
 
-function About() {
+function Work() {
+    const [view,setView] = useState(false)
 
-        const router = useRouter();
-
+    const router = useRouter();
+    useEffect(()=>{
+        setView(false)
+        setTimeout(()=>{
+            setView(true)
+        },1700) //1700
+    },[]);
+    
 
 
   return (
       <div data-scroll-section className="h-screen w-full items-center justify-center flex">
-        <AnimatedCursor
+
+        { view && <AnimatedCursor
       innerSize={15}
       outerSize={8}
       color='0,0,0'
@@ -40,27 +50,36 @@ function About() {
         ".getintouch-circle-footer",
         ".project-item ",
       ]}
-      />
+      />}
+      <div className="h-full w-full " >
+          <div
+          className={` smooth duration-500 ${ !view ?  " hidden bg-[#EEC643]" : " flex bg-transparent"}`} >
+          <Header/>
+        </div>
+
+        {/* List of Works */}
+
     <motion.div
     initial={{
         y:"0",
         scale:1.2,
     }}
     animate={{
-        y:"100vh",
+        y:"110vh",
         scale:1,
     }}
     transition={{
         ease:[0.83, 0, 0.17, 1],
         duration:2.5,
     }}
-
-     className="transition-container scale-125">
+    
+    className="transition-container scale-125">
         {router.pathname.replace("/","")}
     </motion.div>
-      </div>
+         </div>
+        </div>
    
   )
 }
 
-export default About;
+export default Work;
--- pages/contact.js
+++ pages/contact.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { useRouter } from 'next/router';
 import  dynamic  from 'next/dynamic';
 import { motion } from 'framer-motion';
+import Header from '../components/Header/Header';
+import { useEffect } from 'react';
+import { useState } from 'react';
 const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
   ssr: false
-
 });
 
-function Contact() {
-
-     const [view,setView] = useState(false)
+function Work() {
+    const [view,setView] = useState(false)
 
     const router = useRouter();
     useEffect(()=>{
@@ -23,7 +24,8 @@ function Contact() {
 
   return (
       <div data-scroll-section className="h-screen w-full items-center justify-center flex">
-        <AnimatedCursor
+
+        { view && <AnimatedCursor
       innerSize={15}
       outerSize={8}
       color='0,0,0'
@@ -48,27 +50,36 @@ function Contact() {
         ".getintouch-circle-footer",
         ".project-item ",
       ]}
-      />
+      />}
+      <div className="h-full w-full " >
+          <div
+          className={` smooth duration-500 ${ !view ?  " hidden bg-[#EEC643]" : " flex bg-transparent"}`} >
+          <Header/>
+        </div>
+
+        {/* List of Works */}
+
     <motion.div
     initial={{
         y:"0",
         scale:1.2,
     }}
     animate={{
-        y:"100vh",
+        y:"110vh",
         scale:1,
     }}
     transition={{
         ease:[0.83, 0, 0.17, 1],
         duration:2.5,
     }}
-
-     className="transition-container scale-125">
+    
+    className="transition-container scale-125">
         {router.pathname.replace("/","")}
     </motion.div>
-      </div>
+         </div>
+        </div>
    
   )
 }
 
-export default Contact;
+export default Work;
