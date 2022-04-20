54d0dc06a0d6e8c9350929384bc306a81b0b88a4
HazWahb8080 <hazem.wahb100@gmail.com>
2022-04-20 18:29:07 +0200

    final ready

--- components/divider/DividerWorks.jsx
+++ components/divider/DividerWorks.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { ActiveNavState } from './../../atoms/ActiveNavAtom';
 
 function DividerWorks() {
+    const [active,setActive] = useRecoilState(ActiveNavState);
     const router = useRouter();
+    function handleClick () {
+        router.push("/works")
+        setActive("Works")
+    }
+
   return (
     <div className="bg-white w-full h-full mt-60 mb-60 items-center justify-center  flex  py-6 px-6" >
         <h1 
-        onClick={()=>router.push("/works")}
+        onClick={handleClick}
          className="font-Marc font-medium myworks text-9xl h-full w-full text-center hover:scale-105 hover:text-blue-500 smooth" >
             All works
         </h1>
@@ -14,4 +22,4 @@ function DividerWorks() {
   )
 }
 
-export default DividerWorks
+export default DividerWorks;
