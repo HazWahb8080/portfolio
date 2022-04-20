f5d71ecdab87f8e52930e6d6ebb774545c442169
HazWahb8080 <hazem.wahb100@gmail.com>
2022-04-20 18:06:16 +0200

    ready102

--- components/divider/DividerWorks.jsx
+++ components/divider/DividerWorks.jsx
@@ -7,7 +7,7 @@ function DividerWorks() {
     <div className="bg-white w-full h-full mt-60 mb-60 items-center justify-center  flex  py-6 px-6" >
         <h1 
         onClick={()=>router.push("/works")}
-         className="font-Moret myworks text-9xl h-full w-full text-center hover:scale-105 hover:text-blue-500 smooth" >
+         className="font-Marc font-medium myworks text-9xl h-full w-full text-center hover:scale-105 hover:text-blue-500 smooth" >
             All works
         </h1>
     </div>
